25628499abdf381325c7c1681cc33156c9aa5593
Todd Hagler <tdhagler1@gmail.com>
2017-10-01 09:03:25 -0500

    registered admin pages

--- eq_API/admin.py
+++ eq_API/admin.py
@@ -2,3 +2,5 @@ from .models import *
 from django.contrib import admin
 from django.contrib.gis import admin
 admin.site.register(Earthquake_Data, admin.GeoModelAdmin)
+admin.site.register(UserAsset, admin.GeoModelAdmin)
+admin.site.register(Customer)
